a27c486f52389e648ee456a1b20e03e181de6d29
Jonathan Juker <juker0274@gmail.com>
2018-01-18 02:53:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-var name = "happy"
+var name = "Joe"
 var height = "74"
-if (name === "happy") {
-  console.log("this is so")}
-  else {console.log("name should have been happy") }
+
 // Don't worry about this
 module.exports = { name, height
 }
